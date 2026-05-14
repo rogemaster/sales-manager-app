adc0fed168434cea28c1918fdf2ef49b3a3ae88d
rogemaster <rlaaudwn000@naver.com>
2026-05-14 23:16:02 +0900

    feat(order): rebuild search store atoms and update OrderSearchType

--- src/features/order/store/search.store.ts
+++ src/features/order/store/search.store.ts
@@ -2,28 +2,30 @@ import dayjs from 'dayjs';
 import { atom } from 'jotai';
 import { OrderSearchType } from '../types/order.types';
 
-// 필터 기본 상수값
-const DEFAULT_DATE_TYPE: string = 'register';
-const DEFAULT_DATE: Date[] = [dayjs().subtract(7, 'day').toDate(), new Date()];
-const DEFAULT_ORDER_STATUS: string = 'ALL';
+const DEFAULT_DATE_TYPE = 'orderCollectionDate';
+const DEFAULT_START_DATE = dayjs().subtract(7, 'day').format('YYYY-MM-DD');
+const DEFAULT_END_DATE = dayjs().format('YYYY-MM-DD');
+const DEFAULT_ORDER_STATUS = 'ALL';
+const DEFAULT_SEARCH_TYPE = 'orderName';
 
-/**
- * 주문 검색 필터 Atom
- */
 export const dateTypeAtom = atom<string>(DEFAULT_DATE_TYPE);
-
-export const searchDateAtom = atom<Date[]>(DEFAULT_DATE);
-
+export const startDateAtom = atom<string>(DEFAULT_START_DATE);
+export const endDateAtom = atom<string>(DEFAULT_END_DATE);
+export const shoppingMallAtom = atom<string>('ALL');
+export const mallAccountIdAtom = atom<string>('ALL');
+export const deliveryCompanyAtom = atom<string>('ALL');
 export const orderStatusAtom = atom<string>(DEFAULT_ORDER_STATUS);
-
+export const searchTypeAtom = atom<string>(DEFAULT_SEARCH_TYPE);
 export const searchValueAtom = atom<string>('');
 
-/**
- * 주문 검색 필터 데이터
- */
 export const getOrderSearchFilterAtom = atom<OrderSearchType>((get) => ({
   dateType: get(dateTypeAtom),
-  searchDate: get(searchDateAtom),
+  startDate: get(startDateAtom),
+  endDate: get(endDateAtom),
+  shoppingMall: get(shoppingMallAtom),
+  mallAccountId: get(mallAccountIdAtom),
+  deliveryCompany: get(deliveryCompanyAtom),
   orderStatus: get(orderStatusAtom),
+  searchType: get(searchTypeAtom),
   searchValue: get(searchValueAtom),
 }));
